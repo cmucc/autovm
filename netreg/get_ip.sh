d1a08b26e9e7641cb87107bc1554c05ad0656bcf
sbaugh <sbaugh@andrew.cmu.edu>
2014-07-11 19:25:42 -0400

    add netreg

---
+++ netreg/get_ip.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+# TODO do the deletion TODOs as soon as we have reverse DNS
+
+print_usage()
+{
+echo "Usage: get_ip.sh shortname"
+echo "Finds an IP address for shortname.club.cc.cmu.edu, registers it"
+echo "by creating the DNS records, and outputs the IP address."
+}
+
+### Validate arguments
+# check whether $1 is blank
+if [ -z "${1:-}" ]; then 
+    print_usage
+    echo "You did not provide a shortname"
+    exit 1
+fi
+
+# check whether $1 is legal
+if [[ "$1" =~ [^a-zA-Z0-9] ]]; then 
+    print_usage
+    echo "Shortname is illegal under ALMIGHTY CCLUB LAW"
+    exit 1
+fi
+SHORTNAME=$1
+
+### Get Kerberos and AFS permissions
+# check for admin Kerberos token 
+# if not there:
+if [ -z "$(klist | grep admin)" ]; then
+    echo "Run something like \"kinit $USER/admin\", with your Kerberos username."
+    exit 1
+fi
+
+if ! aklog club.cc.cmu.edu; then
+    echo "Failed to get AFS token, do you have AFS?"
+    exit 1
+fi
+
+### Get unused IPs
+# TODO this could be sped up by caching
+UNUSED_LIST_FILE=$(mktemp)
+# for each CClub IP
+
+# TODO uncomment me
+# for i in {1..255}; do 
+#     IP=128.237.157.$i
+#     # if that IP has no reverse DNS
+#     if [ -z "$(dig +short -x $IP)" ]; then
+# 	# put it in the candidate file
+# 	echo "$IP" >> "$UNUSED_LIST_FILE"
+#     fi
+#     # slow down a little 
+#     # is this actually necessary?
+#     sleep .01
+# done  
+
+### Get valid candidate list
+PREREGGED_LIST_FILE=/afs/club.cc.cmu.edu/service/netreg/preregged
+
+### Get the intersection of the two lists
+# (this is the list of IPs that we might be able to use)
+
+# comm requires input to be sorted
+sort $UNUSED_LIST_FILE -o $UNUSED_LIST_FILE
+sort $PREREGGED_LIST_FILE -o $PREREGGED_LIST_FILE
+IP_LIST="$(comm -12 $PREREGGED_LIST_FILE $UNUSED_LIST_FILE)"
+
+# TODO delete these lines
+USED_LIST_FILE=/afs/club.cc.cmu.edu/service/netreg/inuse
+IP_LIST="$(comm -23 $PREREGGED_LIST_FILE $USED_LIST_FILE)"
+
+try_register() 
+{
+    # REQUIRES: we already have admin Kerberos token and have aklog'd for administrator AFS perms
+    cd /afs/club.cc.cmu.edu/service/dns/
+    ./tinydns-edit DB.club.cc.cmu.edu DB.club.cc.cmu.edu.tmp add host $SHORTNAME.club.cc.cmu.edu $1
+}
+
+for IP in $IP_LIST;
+do
+    if (try_register $IP); then
+	printf $IP
+	# TODO delete this line
+	echo $IP >> $USED_LIST_FILE
+	exit 0
+    fi
+done
+
+echo "Eh... it looks like there are no IPs available..."
+echo "Might want to preregister some more and add them to:"
+echo "$PREREGGED_LIST_FILE"
+echo "Alternatively email sbaugh?"
+exit 1
