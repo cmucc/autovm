014419d9a849ac2abdb04713f18d8c77b4c62326
sbaugh <sbaugh@andrew.cmu.edu>
2014-07-11 18:57:48 -0400

    add get_ip.sh usage

--- make_vm.sh
+++ make_vm.sh
@@ -1,38 +1,17 @@
 #!/bin/bash
-# this script is really just a lame wrapper around virt-install
 set -o errexit
 set -o nounset
 
 #VM_HOSTNAME=
-#VM_IP=128.237.157.
 #VM_CCLUB=yes
 #VM_ROOT_PASSWORD_HASH=$(printf "password" | mkpasswd -s -m md5)
 NUMCPUS=1
 MEMORY=512
 DISKSIZE=8
 
-DEBIAN_LOCATION=http://ftp.us.debian.org/debian/dists/wheezy/main/installer-amd64/
-PRESEED_MAIN_TEMPLATE=/root/creation_scripts/materials/debian7_preseed.cfg
-
-# this root pubkey should be in a package! that'd be cool!
-# TODO make a package with it! yeah! cclub-keyring, perhaps!
-CCLUB_ROOT_PUBKEY=/root/creation_scripts/materials/cclub_root.pub
-
 while [ -z "${VM_HOSTNAME:-}" ]; do
     read -e -p "VM hostname: " VM_HOSTNAME
 done
-
-while [ -z "${VM_IP:-}" ]; do
-    read -e -i "128.237.157." -p "VM IP address: " VM_IP
-    LASTOCTET=$(echo $VM_IP | cut -d . -f 4)
-    if [[ ! "$LASTOCTET" =~ ^1?[0-9][0-9]$ ]]; then
-	echo "You've entered the IP wrong, giving \"$LASTOCTET\" as the last octet." 
-	unset VM_IP
-    fi
-done
-LASTOCTET=$(echo $VM_IP | cut -d . -f 4)
-
-MAC_ADDRESS=00:00:80:ed:9d:$(printf '%x' $LASTOCTET)
 
 while [ -z "${VM_CCLUB:-}" ]; do
     read -e -p "Clubify this VM (yes/no)? " VM_CCLUB
@@ -41,11 +20,29 @@ done
 while [ -z "${VM_ROOT_PASSWORD_HASH:-}" ]; do
     echo "Enter a root password:"
     echo "WARNING! WILL BE DISPLAYED! HIDE YO KIDS, HIDE YO TERMINAL!"
-    # displayed deliberately because it would be annoying as HECK to typo
     read -e VM_PASSWORD
     VM_ROOT_PASSWORD_HASH=$(echo "$VM_PASSWORD" | mkpasswd -s -m md5)
     unset VM_PASSWORD
 done
+
+echo "You have reached the point of no return."
+echo "Further progress will make changes to the state of the world!"
+echo "If you want to be non-interactive past here, make sure you have"
+echo "run \"kinit \$YOUR_USERNAME/admin\" and \"aklog\"."
+read -p "Will now attempt to claim an IP, continue (yes/no)? " choice
+case "$choice" in 
+  yes|YES ) echo "Okay";;
+  * ) exit 1;;
+esac
+VM_IP=$(/afs/club.cc.cmu.edu/service/netreg/get_ip.sh $VM_HOSTNAME)
+
+#### Create VM
+DEBIAN_LOCATION=http://ftp.us.debian.org/debian/dists/wheezy/main/installer-amd64/
+PRESEED_MAIN_TEMPLATE=/root/creation_scripts/materials/debian7_preseed.cfg
+CCLUB_ROOT_PUBKEY=/root/creation_scripts/materials/cclub_root.pub
+
+LASTOCTET=$(echo $VM_IP | cut -d . -f 4)
+MAC_ADDRESS=00:00:80:ed:9d:$(printf '%x' $LASTOCTET)
 
 echo "Now creating a VM with:"
 echo "$NUMCPUS CPU cores"
@@ -55,13 +52,6 @@ echo "$VM_HOSTNAME as a hostname"
 echo "$VM_IP as an IP address"
 echo "$MAC_ADDRESS as a MAC address"
 echo "$VM_CCLUB to being Clubified"
-
-read -p "Continue (yes/no)? " choice
-case "$choice" in 
-  yes|YES ) echo "Okay, starting install.";;
-  * ) exit;;
-esac
-
 echo "Connect with \"virsh console $VM_HOSTNAME\" elsewhere to watch \
 the install progress."
 
@@ -70,6 +60,7 @@ TMPDIR=$(mktemp -d)
 cp $PRESEED_MAIN_TEMPLATE $TMPDIR/preseed.cfg 
 sed -i "s/REPLACE_WITH_IP/$VM_IP/" $TMPDIR/preseed.cfg 
 sed -i "s/REPLACE_WITH_HOSTNAME/$VM_HOSTNAME/" $TMPDIR/preseed.cfg
+# gotta escape the hash - it could have character combos special to sed in it
 sed -i "s/REPLACE_WITH_ROOTPW_HASH/$(echo $VM_ROOT_PASSWORD_HASH | sed -e 's/[\/&]/\\&/g')/" $TMPDIR/preseed.cfg
 
 virt-install \
@@ -85,14 +76,17 @@ virt-install \
     --initrd-inject=$TMPDIR/preseed.cfg \
     --initrd-inject=$CCLUB_ROOT_PUBKEY
 
+#### Perform clubification
 CCLUB_ROOT_PRVKEY=/root/secret/cclub_root
 CCLUB_SECRETS=/root/creation_scripts/secret/
 CCLUB_CLUBIFY_SCRIPT=/root/creation_scripts/clubify.sh
 
 if [[ "$VM_CCLUB" == "yes" ]]; then
     echo "Waiting for VM to reboot before starting Clubification..."
-    sleep 15
+    sleep 30
     # TODO this secret dir should live in AFS
     scp -o StrictHostKeyChecking=no -i $CCLUB_ROOT_PRVKEY -r $CCLUB_SECRETS root@$VM_IP:/root/
     ssh -o StrictHostKeyChecking=no -i $CCLUB_ROOT_PRVKEY root@$VM_IP "bash -s" < $CCLUB_CLUBIFY_SCRIPT
 fi
+
+echo "Done!"
