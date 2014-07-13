f51308bc8b5c0c407dbb6bb5c60744ebeea65e65
sbaugh <sbaugh@andrew.cmu.edu>
2014-07-12 21:29:49 -0400

    small userfriendliness improvements

--- make_vm_wheezy.sh
+++ make_vm_wheezy.sh
@@ -37,12 +37,12 @@ case "$choice" in
   yes|YES ) echo "Okay";;
   * ) exit 1;;
 esac
+
+#### Get an IP
 VM_IP=$($AUTOVM_DIR/netreg/get_ip.sh $VM_HOSTNAME)
 
 #### Create VM
-DEBIAN_LOCATION=http://ftp.us.debian.org/debian/dists/wheezy/main/installer-amd64/
 PRESEED_MAIN_TEMPLATE=$AUTOVM_DIR/materials/debian7_preseed.cfg
-CCLUB_ROOT_PUBKEY=$AUTOVM_DIR/materials/cclub_root.pub
 
 LASTOCTET=$(echo $VM_IP | cut -d . -f 4)
 MAC_ADDRESS=00:00:80:ed:9d:$(printf '%x' $LASTOCTET)
@@ -57,6 +57,7 @@ echo "$MAC_ADDRESS as a MAC address"
 echo "$VM_CCLUB to being Clubified"
 echo "Connect with \"virsh console $VM_HOSTNAME\" elsewhere to watch \
 the install progress."
+sleep 1
 
 # make the preseed.cfg
 TMPDIR=$(mktemp -d)
@@ -65,6 +66,9 @@ sed -i "s/REPLACE_WITH_IP/$VM_IP/" $TMPDIR/preseed.cfg
 sed -i "s/REPLACE_WITH_HOSTNAME/$VM_HOSTNAME/" $TMPDIR/preseed.cfg
 # gotta escape the hash - it could have character combos special to sed in it
 sed -i "s/REPLACE_WITH_ROOTPW_HASH/$(echo $VM_ROOT_PASSWORD_HASH | sed -e 's/[\/&]/\\&/g')/" $TMPDIR/preseed.cfg
+
+DEBIAN_LOCATION=http://ftp.us.debian.org/debian/dists/wheezy/main/installer-amd64/
+CCLUB_ROOT_PUBKEY=$AUTOVM_DIR/materials/cclub_root.pub
 
 virt-install \
     --noautoconsole \
@@ -84,12 +88,14 @@ CCLUB_ROOT_PRVKEY=$CCLUB_SECRETS/cclub_root
 CCLUB_CLUBIFY_SCRIPT=$AUTOVM_DIR/clubify_wheezy.sh
 
 if [[ "$VM_CCLUB" == "yes" ]]; then
-    echo "Waiting for VM to reboot before starting Clubification..."
+    printf "Waiting for VM to start before starting Clubification..."
     sleep 30
-    scp -o StrictHostKeyChecking=no -i $CCLUB_ROOT_PRVKEY -r $CCLUB_SECRETS root@$VM_IP:/root/
+    echo "let's go!"
     ssh -o StrictHostKeyChecking=no -i $CCLUB_ROOT_PRVKEY root@$VM_IP "bash -s" < $CCLUB_CLUBIFY_SCRIPT
 fi
 
 echo "Done!"
-echo "Connect to the VM over ssh at $VM_HOSTNAME.club.cc.cmu.edu."
-echo "Or, at $VM_IP."
+echo "Wait a few seconds for the VM to reboot."
+echo "Then, connect to the VM over ssh at any of:"
+echo "$VM_HOSTNAME.club.cc.cmu.edu"
+echo "$VM_IP"
